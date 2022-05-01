bb630f7d8c7d69d91b3d6b9d359d529547ea276f
Andrewwxo <74658753+Andrewwxo@users.noreply.github.com>
2022-05-02 00:57:17 +0300

    Create PropsType

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Body } from './Site/Body';
+import {Header} from './Site/Header';
+import {Footer} from './Site/Footer';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+    <>
+        <Header title={'Hi!'} />
+        <Body titleForBody={'New Body'}/>
+        <Footer titleForFooter={'New Footer'}/>
+
+    </>
   );
 }
 
