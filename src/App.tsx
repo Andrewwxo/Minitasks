320dbf4117a14ffc75b2aca33d9b313a1b4cf22e
Andrewwxo <74658753+Andrewwxo@users.noreply.github.com>
2022-05-05 00:43:03 +0300

    task input/button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React, {useLayoutEffect, useState} from 'react';
 import './App.css';
-import {Body} from './Site/Body';
-import {Header} from './Site/Header';
-import {Footer} from './Site/Footer';
 import {NewComponent, topCars} from './Site/NewComponent';
 import {Button} from './components/Button';
-import {FilterType, Money} from './components/Money';
+import {Money} from './components/Money';
+import {FullInput} from './components/FullInput';
+import {message} from 'antd';
+import {Input} from './components/Input';
+import {ButtonInput} from './components/ButtonInput';
 
 
 function App() {
 
-    let[a,setA]=useState(0)
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+    ])
+    let [title, setTitle] = useState('')
+
+    const addMessage = (title: string) => {
+        let newMessage = {message: title};
+        setMessage([newMessage, ...message])
+    }
+
+
+    let [a, setA] = useState(0)
 
     const clickHandler = (num: number) => {
         setA(++a);
@@ -20,17 +34,29 @@ function App() {
         setA(0);
     }
 
+    const callBackButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
+    }
+
     return (
         <>
             <NewComponent topCars={topCars}/>
-            <Button name={'Number'} callBack={()=>clickHandler(0)} />
-            <Button name={'0'} callBack={()=>clickHandlerNull(0)} />
+            <Button name={'Number'} callBack={() => clickHandler(0)}/>
+            <Button name={'0'} callBack={() => clickHandlerNull(0)}/>
             <h1>{a}</h1>
-            <Money />
+            <Money/>
+
+            <Input setTitle={setTitle} title={title}/>
+            <ButtonInput name={'+'} callBack={callBackButtonHandler}/>
+            {/*<FullInput addMessage={addMessage}/>*/}
+            {message.map((el, index) => {
+                return (<div key={index}>
+                    {el.message}
+                </div>)
+            })}
 
         </>
-
-
     )
         ;
 }
